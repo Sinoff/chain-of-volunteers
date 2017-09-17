f89762b66ec9c1b34b30fd09fc405a1a3e9d5522
Roi <sinoff@campus.technion.ac.il>
2017-09-17 05:44:18 +0300

    route & segments begin

--- app/src/main/java/chainofvolunteers/chainofvolunteers/AppHelpers/Route.java
+++ app/src/main/java/chainofvolunteers/chainofvolunteers/AppHelpers/Route.java
@@ -1,12 +1,54 @@
 package chainofvolunteers.chainofvolunteers.AppHelpers;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.LinkedList;
+
+
+
 /**
  * Created by madeline on 9/16/17.
  */
 
 public class Route {
-    public Route (int start_point, int end_point)
-    {
 
+    LinkedList<Segment> segments;
+
+    public Route ()
+    {
+        segments = new LinkedList<>();
     }
+
+    public void jsonToList(JSONObject obj)
+    {
+        JSONArray jsonSegments = null;
+        try {
+            jsonSegments = obj.getJSONArray("routes").getJSONArray(3);
+            for (int i=0; i < jsonSegments.length(); i++)
+            {
+                segments.add(new Segment
+                        (new LatLng(
+                                jsonSegments.getJSONObject(i).getJSONObject("start_location").getDouble("lat"),
+                                jsonSegments.getJSONObject(i).getJSONObject("start_location").getDouble("lng"))
+                                ,
+                        new LatLng(
+                                jsonSegments.getJSONObject(i).getJSONObject("end_location").getDouble("lat"),
+                                jsonSegments.getJSONObject(i).getJSONObject("end_location").getDouble("lng"))
+                        ));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("JSONExample.json"));
+    }
+    // typecasting obj to JSONObject
+    JSONObject jo = (JSONObject) obj;
 }
--- app/src/main/java/chainofvolunteers/chainofvolunteers/AppHelpers/Segment.java
+++ app/src/main/java/chainofvolunteers/chainofvolunteers/AppHelpers/Segment.java
@@ -1,9 +1,19 @@
 package chainofvolunteers.chainofvolunteers.AppHelpers;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by madeline on 9/16/17.
  */
 
 public class Segment {
 
+    LatLng      startPoint;
+    LatLng      endPoint;
+
+    public Segment(LatLng start, LatLng end)
+    {
+        startPoint = start;
+        endPoint = end;
+    }
 }
